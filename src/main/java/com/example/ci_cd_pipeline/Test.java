a07e74cb32a2f9aa52f872d038732045fa6432ad
akramjonovazamjon <mrazamjonovakramjon@gmail.com>
2023-04-05 13:20:20 +0500

    add

--- src/main/java/com/example/ci_cd_pipeline/Test.java
+++ src/main/java/com/example/ci_cd_pipeline/Test.java
@@ -12,7 +12,7 @@ public class Test {
 
     @GetMapping("/list")
     public List<String > user(){
-        return List.of("Azamjon", "Ravshanbek");
+        return List.of("Azamjon", "Ravshanbek", "Odilbek");
     }
 
 }
