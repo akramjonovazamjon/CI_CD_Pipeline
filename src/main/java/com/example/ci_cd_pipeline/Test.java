0bcb6872bc7ef6f081652689d90703de1aad1968
akramjonovazamjon <mrazamjonovakramjon@gmail.com>
2023-04-05 12:52:34 +0500

    Initial commit

---
+++ src/main/java/com/example/ci_cd_pipeline/Test.java
@@ -0,0 +1,18 @@
+package com.example.ci_cd_pipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController("/user")
+public class Test {
+
+
+    @GetMapping("/list")
+    public List<String > user(){
+        return List.of();
+    }
+
+}
